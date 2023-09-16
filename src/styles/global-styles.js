60f9b95d235df250adbf3f8aa2eae06530451def
Gustavo Maia <ms.gustavo@outlook.com>
2023-09-16 19:12:34 -0300

    Configurando fontes

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,10 +1,24 @@
 import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,900;1,900&family=Open+Sans:wght@700&family=Poppins:ital,wght@0,300;1,300&display=swap');
+
 *{
   margin: 0;
   padding: 0;
 }
 
+html {
+  font-size: 62.5%;
+}
+
 body{
-}`;
+  font-size: 1.6rem;
+  font-family: ${({ theme }) => theme.font.family.default}
+}
+
+h1 {
+  font-family: ${({ theme }) => theme.font.family.secondary}
+}
+`;
