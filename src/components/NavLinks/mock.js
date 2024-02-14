73923a30299a86aaecc7a276aee49573480894dc
Gustavo Maia <ms.gustavo@outlook.com>
2024-02-14 10:32:29 -0300

    NavLinks

---
+++ src/components/NavLinks/mock.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    children: 'Link 1',
+    link: '#target 1',
+    newTab: false,
+  },
+  {
+    children: 'Link 2',
+    link: '#target 2',
+    newTab: false,
+  },
+  {
+    children: 'Link 3',
+    link: '#target 3',
+    newTab: false,
+  },
+  {
+    children: 'Link 4',
+    link: '#target 4',
+    newTab: false,
+  },
+  {
+    children: 'Link 5',
+    link: '#target 5',
+    newTab: false,
+  },
+  {
+    children: 'Link 6',
+    link: '#target 6',
+    newTab: false,
+  },
+  {
+    children: 'Link 7',
+    link: '#target 7',
+    newTab: false,
+  },
+];
