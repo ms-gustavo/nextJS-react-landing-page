e739734b98716af7e1b6015a1d18cfb1446ca034
Gustavo Maia <ms.gustavo@outlook.com>
2024-03-07 11:19:14 -0300

    Component GridImage

---
+++ src/components/GridImage/GridImage.test.jsx
@@ -0,0 +1,16 @@
+import { screen } from '@testing-library/react';
+import { GridImage } from '.';
+import { renderTheme } from '../../styles/render-theme';
+import mock from './mock';
+
+describe('<GridImage/>', () => {
+  it('should render with background', () => {
+    const { container } = renderTheme(<GridImage {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render withou background', () => {
+    const { container } = renderTheme(<GridImage {...mock} background={undefined} />);
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/components/GridImage/mock.js
@@ -0,0 +1,32 @@
+export default {
+  background: false,
+  title: 'My grid image',
+  description:
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident harum quo esse cupiditate et ab optio debitis, est facilis in deleniti inventore vel aperiam cumque consequuntur nesciunt nemo qui necessitatibus.',
+  grid: [
+    {
+      altText: 'Teste 1',
+      srcImg: 'https://source.unsplash.com/random/800x800',
+    },
+    {
+      altText: 'Teste 2',
+      srcImg: 'https://source.unsplash.com/random/800x800',
+    },
+    {
+      altText: 'Teste 3',
+      srcImg: 'https://source.unsplash.com/random/800x800',
+    },
+    {
+      altText: 'Teste 4',
+      srcImg: 'https://source.unsplash.com/random/800x800',
+    },
+    {
+      altText: 'Teste 5',
+      srcImg: 'https://source.unsplash.com/random/800x800',
+    },
+    {
+      altText: 'Teste 6',
+      srcImg: 'https://source.unsplash.com/random/800x800',
+    },
+  ],
+};
