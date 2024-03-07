a584a7cde63dbc5ac9c8c8fd456da66b52c63597
Gustavo Maia <ms.gustavo@outlook.com>
2024-03-07 11:34:36 -0300

    Footer component

---
+++ src/components/Footer/stories.jsx
@@ -0,0 +1,17 @@
+import { Footer } from '.';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+  args: {
+    html: `<p><a href="https://github.com/ms-gustavo">Feito por Gustavo Maia</a></p>`,
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <Footer {...args} />
+    </div>
+  );
+};
