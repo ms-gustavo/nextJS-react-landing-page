31d24bff67dbddf935546608ceab8d1cdf4c4499
Gustavo Maia <ms.gustavo@outlook.com>
2024-03-07 10:48:06 -0300

    componente de GridText

---
+++ src/components/GridText/GridText.test.jsx
@@ -0,0 +1,16 @@
+import { screen } from '@testing-library/react';
+import { GridText } from '.';
+import { renderTheme } from '../../styles/render-theme';
+import mock from './mock';
+
+describe('<GridText/>', () => {
+  it('should render with background', () => {
+    const { container } = renderTheme(<GridText {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render withou background', () => {
+    const { container } = renderTheme(<GridText {...mock} background={undefined} />);
+    expect(container).toMatchSnapshot();
+  });
+});
--- src/components/GridText/index.jsx
+++ src/components/GridText/index.jsx
@@ -3,18 +3,20 @@ import * as Styled from './styles';
 import { SectionBackground } from '../SectionBackground';
 import { Heading } from '../Heading';
 import { TextComponent } from '../TextComponent';
-export const GridSection = ({ title, description, grid }) => {
+export const GridText = ({ title, description, grid, background = false }) => {
   return (
-    <SectionBackground>
+    <SectionBackground background={background}>
       <Styled.Container>
-        <Heading size="huge" uppercase>
+        <Heading size="huge" uppercase colorDark={!background}>
           {title}
         </Heading>
         <TextComponent>{description}</TextComponent>
         <Styled.Grid>
           {grid.map((element) => (
             <Styled.GridElement key={element.title}>
-              <Heading size="medium">{element.title}</Heading>
+              <Heading size="medium" colorDark={!background}>
+                {element.title}
+              </Heading>
               <TextComponent>{element.description}</TextComponent>
             </Styled.GridElement>
           ))}
@@ -24,7 +26,8 @@ export const GridSection = ({ title, description, grid }) => {
   );
 };
 
-GridSection.propTypes = {
+GridText.propTypes = {
+  background: P.bool,
   title: P.string.isRequired,
   description: P.string.isRequired,
   grid: P.arrayOf(
