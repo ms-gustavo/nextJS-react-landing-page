d5c7b5f28d1c6d1f99df4cd9c9d17813d1871c18
Gustavo Maia <ms.gustavo@outlook.com>
2024-03-13 08:54:35 -0300

    Mapeando Section Two Columns

--- src/api/map-data.js
+++ src/api/map-data.js
@@ -1,3 +1,5 @@
+import { mapSections } from './map-sections';
+import { mapMenu } from './map-menu';
 export const mapData = (pagesData = [{}]) => {
   return pagesData.map((data) => {
     const { footer_text: footerHtml = '', slug = '', title = '', sections = [], menu = {} } = data;
@@ -6,8 +8,8 @@ export const mapData = (pagesData = [{}]) => {
       footerHtml,
       slug,
       title,
-      sections,
-      menu,
+      sections: mapSections(sections),
+      menu: mapMenu(menu),
     };
   });
 };
---
+++ src/api/map-sections.test.js
@@ -0,0 +1,66 @@
+import { mapSectionTwoColumns, mapSections } from './map-sections';
+
+describe('map-sections', () => {
+  it('should render predefined section if no data ', () => {
+    const data = mapSections();
+    expect(data).toEqual([]);
+  });
+
+  it('should map section two columns withtout data', () => {
+    const data = mapSectionTwoColumns();
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.srcImg).toBe('');
+    expect(data.text).toBe('');
+    expect(data.title).toBe('');
+  });
+
+  it('should map section two columns with data', () => {
+    const data = mapSectionTwoColumns({
+      __component: 'section.section-two-columns',
+      _id: '602fdf2d540c00269e056172',
+      title: 'Title',
+      description: 'Test',
+      metadata: {
+        background: true,
+        _id: '602fdf2d540c00269e05617f',
+        section_id: 'home',
+        name: 'Home',
+        __v: 0,
+        id: '602fdf2d540c00269e05617f',
+      },
+      __v: 1,
+      image: {
+        _id: '602fdc2b540c00269e05616a',
+        name: 'javascript.svg',
+        alternativeText: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
+        caption: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
+        hash: 'javascript_b57bf48cda',
+        ext: '.svg',
+        mime: 'image/svg+xml',
+        size: 30.31,
+        url: 'a.svg',
+        provider_metadata: {
+          public_id: 'javascript_b57bf48cda',
+          resource_type: 'image',
+        },
+        provider: 'cloudinary',
+        width: 1030,
+        height: 730,
+        related: ['602fdf2d540c00269e056178', '602fdf2d540c00269e056172'],
+        createdAt: '2021-02-19T15:41:31.808Z',
+        updatedAt: '2021-02-19T15:54:22.590Z',
+        __v: 0,
+        id: '602fdc2b540c00269e05616a',
+      },
+      id: '602fdf2d540c00269e056172',
+    });
+    expect(data.background).toBe(true);
+    expect(data.component).toBe('section.section-two-columns');
+    expect(data.sectionId).toBe('home');
+    expect(data.srcImg).toBe('a.svg');
+    expect(data.text).toBe('Test');
+    expect(data.title).toBe('Title');
+  });
+});
