268ece78c6d09508ce5ad3332b4e4531fa473a74
Gustavo Maia <ms.gustavo@outlook.com>
2024-03-13 10:11:28 -0300

    Finalizando testes

--- src/api/map-sections.js
+++ src/api/map-sections.js
@@ -7,7 +7,7 @@ export const mapSections = (sections = []) => {
       return mapSectionContent(section);
     }
     if (section.__component === 'section.section-grid') {
-      const { __component: { text_grid = [], image_grid = [] } = '' } = section;
+      const { text_grid = [], image_grid = [] } = section;
 
       if (text_grid.length > 0) {
         return mapTextGrid(section);
@@ -71,7 +71,13 @@ export const mapTextGrid = (section = {}) => {
     title,
     description,
     background,
-    grid,
+    grid: grid.map((text) => {
+      const { title = '', description = '' } = text;
+      return {
+        title,
+        description,
+      };
+    }),
     sectionId,
   };
 };
--- src/api/map-sections.test.js
+++ src/api/map-sections.test.js
@@ -1,9 +1,43 @@
 import { mapImageGrid, mapSectionContent, mapSectionTwoColumns, mapSections, mapTextGrid } from './map-sections';
 
+import pagesFakeData from './dados.json';
+
 describe('map-sections', () => {
+  it('should render predefined section if data ', () => {
+    const data = mapSections(pagesFakeData[0].sections);
+    expect(data[0].component).toBe('section.section-two-columns');
+  });
+
   it('should render predefined section if no data ', () => {
     const data = mapSections();
     expect(data).toEqual([]);
+  });
+
+  it('should test section with invalid data', () => {
+    const withNoTextOrImageGrid = mapSections([
+      {
+        __component: 'section.section-grid',
+      },
+    ]);
+
+    const withNoComponent = mapSections([{}]);
+    expect(withNoTextOrImageGrid).toEqual([{ __component: 'section.section-grid' }]);
+    expect(withNoComponent).toEqual([{}]);
+  });
+
+  it('should test section grid with no text_grid or image_grid', () => {
+    const withNoTextOrImageGrid = mapSections([
+      {
+        __component: 'section.section-grid',
+        image_grid: [{}],
+      },
+      {
+        __component: 'section.section-grid',
+        text_grid: [{}],
+      },
+    ]);
+
+    expect(withNoTextOrImageGrid.length).toBe(2);
   });
 
   it('should map section two columns withtout data', () => {
